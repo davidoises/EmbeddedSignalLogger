2275b7d9c62ab82b680b14718817452c1e7f9fd2
davidoises <davidoises@hotmail.com>
2024-07-30 12:36:47 -0700

    Continuing to develop the initial configuration of the system

--- C2000/embedded_app/drivers/device.c
+++ C2000/embedded_app/drivers/device.c
@@ -1,12 +1,90 @@
 #include "device.h"
 // #include "driverlib.h"
+#include <stddef.h>
 
 #include "../third_party/driverlib/sysctl.h"
+#include "../third_party/driverlib/flash.h"
+
+
+//
+// 20MHz XTAL on controlCARD and Launchpad. For use with SysCtl_getClock().
+//
+#define DEVICE_OSCSRC_FREQ          20000000U
+
+//
+// Define to pass to SysCtl_setClock(). Will configure the clock as follows:
+// PLLSYSCLK = 20MHz (XTAL_OSC) * 10 (IMULT) * 1 (FMULT) / 2 (PLLCLK_BY_2)
+//
+#define DEVICE_SETCLOCK_CFG         (SYSCTL_OSCSRC_XTAL | SYSCTL_IMULT(10) |  \
+                                     SYSCTL_FMULT_NONE | SYSCTL_SYSDIV(2) |   \
+                                     SYSCTL_PLL_ENABLE)
+
+//
+// 100MHz SYSCLK frequency based on the above DEVICE_SETCLOCK_CFG. Update the
+// code below if a different clock configuration is used!
+//
+#define DEVICE_SYSCLK_FREQ          ((DEVICE_OSCSRC_FREQ * 10 * 1) / 2)
+
+//
+// 25MHz LSPCLK frequency based on the above DEVICE_SYSCLK_FREQ and a default
+// low speed peripheral clock divider of 4. Update the code below if a
+// different LSPCLK divider is used!
+//
+#define DEVICE_LSPCLK_FREQ          (DEVICE_SYSCLK_FREQ / 4)
+
+//*****************************************************************************
+//
+// Macro to call SysCtl_delay() to achieve a delay in microseconds. The macro
+// will convert the desired delay in microseconds to the count value expected
+// by the function. \b x is the number of microseconds to delay.
+//
+//*****************************************************************************
+#define DEVICE_DELAY_US(x) SysCtl_delay(((((long double)(x)) / (1000000.0L /  \
+                              (long double)DEVICE_SYSCLK_FREQ)) - 9.0L) / 5.0L)
+
+#define DEVICE_FLASH_WAITSTATES 4
+
+extern uint16_t RamfuncsLoadStart;
+extern uint16_t RamfuncsRunStart;
+extern uint16_t RamfuncsLoadSize;
 
 void device_init(void)
 {
     // Disable the watchdog
     SysCtl_disableWatchdog();
 
+    //
+    // Copy time critical code and flash setup code to RAM. This includes the
+    // following functions: InitFlash();
+    //
+    // The RamfuncsLoadStart, RamfuncsLoadSize, and RamfuncsRunStart symbols
+    // are created by the linker. Refer to the device .cmd file.
+    //
+    memcpy(&RamfuncsRunStart, &RamfuncsLoadStart, (size_t)&RamfuncsLoadSize);
 
+    //
+    // Call Flash Initialization to setup flash waitstates. This function must
+    // reside in RAM.
+    //
+    Flash_initModule(FLASH0CTRL_BASE, FLASH0ECC_BASE, DEVICE_FLASH_WAITSTATES);
+
+    //
+    // Set up PLL control and clock dividers
+    //
+    SysCtl_setClock(DEVICE_SETCLOCK_CFG);
+
+    //
+    // Make sure the LSPCLK divider is set to the default (divide by 4)
+    //
+    SysCtl_setLowSpeedClock(SYSCTL_LSPCLK_PRESCALE_4);
+
+    //
+    // These asserts will check that the #defines for the clock rates in
+    // device.h match the actual rates that have been configured. If they do
+    // not match, check that the calculations of DEVICE_SYSCLK_FREQ and
+    // DEVICE_LSPCLK_FREQ are accurate. Some examples will not perform as
+    // expected if these are not correct.
+    //
+    ASSERT(SysCtl_getClock(DEVICE_OSCSRC_FREQ) == DEVICE_SYSCLK_FREQ);
+    ASSERT(SysCtl_getLowSpeedClock(DEVICE_OSCSRC_FREQ) == DEVICE_LSPCLK_FREQ);
 }
